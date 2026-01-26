74898bb6d89c22dfd6e8977e3322436b6b90ad4b
gobika-921 <radhagobika921@gmail.com>
2026-01-26 15:53:33 +0530

    Clarify features and labels in apple_orange.py
    Added comment to clarify features and labels.

--- ML-codes/apple_orange.py
+++ ML-codes/apple_orange.py
@@ -1,5 +1,5 @@
 from sklearn import tree
-features = [[150 , 1], [170, 1], [140 , 0], [130, 0], [190, 1], [120, 0]]
+features = [[150 , 1], [170, 1], [140 , 0], [130, 0], [190, 1], [120, 0]] # features are weight in grams and texture , 1 -> bumpy , 0 -> smooth 
 labels = [1, 1, 0, 0, 1, 0]  # 1 for orange, 0 for apple
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
