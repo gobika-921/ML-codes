d0f8e9cd91f00103adde0ef98bbc60467c91ac7e
Gobika-921 <radhagobika921@gmail.com>
2026-01-26 15:48:31 +0530

    Add ML codes

---
+++ ML-codes/apple_orange.py
@@ -0,0 +1,6 @@
+from sklearn import tree
+features = [[150 , 1], [170, 1], [140 , 0], [130, 0], [190, 1], [120, 0]]
+labels = [1, 1, 0, 0, 1, 0]  # 1 for orange, 0 for apple
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print (clf.predict([[190, 1]]))  # Predict for a fruit with weight 190 and bumpy texture
