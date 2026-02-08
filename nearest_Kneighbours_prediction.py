c18c69d917bf09d1f003b060efedb6e87e324e5e
Gobika B <radhagobika921@gmail.com>
2026-02-08 13:01:46 +0530

    prediction using K-nearest Neighbours classifier

---
+++ nearest_Kneighbours_prediction.py
@@ -0,0 +1,17 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+
+x = iris.data
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.5)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(x_train, y_train)
+prediction = knn.predict(x_test)
+print(prediction)
+   
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, prediction))
